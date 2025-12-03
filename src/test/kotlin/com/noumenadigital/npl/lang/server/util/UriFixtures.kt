7639875c18d26edc78e0817fdb8b7f0c74c8b1f8
mozzerin <mikhail@noumenadigital.com>
2025-12-03 11:16:43 +0100

    feat: update Java version to 24 and add Windows path normalization for URIs

--- src/test/kotlin/com/noumenadigital/npl/lang/server/util/UriFixtures.kt
+++ src/test/kotlin/com/noumenadigital/npl/lang/server/util/UriFixtures.kt
@@ -1,16 +1,29 @@
 package com.noumenadigital.npl.lang.server.util
 
+import org.apache.commons.lang3.StringUtils.replace
+import java.io.File
 import java.net.URI
 
 object UriFixtures {
     fun normalizeUri(uriString: String): String {
-        val uri = URI.create(uriString)
+        val normalizeWindowsPath = normalizeWindowsPath(uriString)
+        val uri = URI.create(normalizeWindowsPath)
         return if (uri.scheme == "file") {
             val path = uri.path
             val cleanPath = path.dropWhile { it == '/' }
             "file:///$cleanPath"
         } else {
-            uriString
+            normalizeWindowsPath
         }
+    }
+
+    fun normalizeWindowsPath(uri: String): String {
+        if (File.separatorChar != '\\') {
+            return uri
+        }
+
+        return uri.replace(Regex("/([A-Za-z]):/")) { match ->
+            "${match.groupValues[1]}:\\"
+        }.replace('/', '\\')
     }
 }
