1ba55fc6f0b99e477626a3fd6cd418dc39848e89
mozzerin <mikhail@noumenadigital.com>
2025-12-03 08:16:04 +0100

    feat: add native image configuration validation and update build scripts

---
+++ .github/scripts/validate-native-config-files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "Generating native configuration with Maven..."
+./generate-native-configs.sh
+
+echo "Validating native-image configs..."
+if [[ -n $(git diff --ignore-all-space -- src/main/resources/META-INF/native-image/) ]]; then
+  echo "Error: Unexpected changes detected in native-image configs:"
+  git diff --ignore-all-space -- src/main/resources/META-INF/native-image/
+  exit 1
+else
+  echo "Validation passed: No changes detected in native-image configuration."
+  exit 0
+fi
--- generate-native-configs.sh
+++ generate-native-configs.sh
@@ -3,10 +3,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-echo "Building fat jar..."
-mvn clean package -DskipTests -PbuildFatjar
-
 echo "Generating native image configurations..."
-java -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image \
-  -cp target/language-server-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-  com.noumenadigital.npl.lang.server.MainKt
+mvn clean verify -Pconfig-gen
+
